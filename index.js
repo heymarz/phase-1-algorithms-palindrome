56b1759720c49dff6c7b955f91a5a5c0bb3d81c7
Marlena Tsang <Marlena345@gmail.com>
2021-10-21 15:27:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function reverse(word) {
+  return word.split("").reverse().join("")
 }
+
+function isPalindrome(word= racecar) {
+  const reverseWord = reverse(word)
+  return word === reverseWord
+}
+
+
+
 
 /* 
   Add your pseudocode here
+  character matches: char[0] === char [-1]; char(1) ===char(-2) then return true
 */
 
 /*
   Add written explanation of your solution here
-*/
+const str = 'mañana mañana'
+const strReverse = str.split(0,).reverse().join('')
+// => "anãnam anañam" // notice how the first word has an ã rather ñ*/
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
